25ef336aab0e3a7035e8094efdbc3693151897aa
TAHA SELLAK <sellaktaha1@gmail.com>
2024-09-11 16:17:52 +0100

    Générer les composants la connexion et l'inscription.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { SignupComponent } from './authentication/signup/signup.component';
+import { SigninComponent } from './authentication/signin/signin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthenticationComponent,
+    SignupComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
