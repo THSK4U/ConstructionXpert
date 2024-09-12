dbb70dca89e510e6107cec67ed97dd4c0c247450
TAHA SELLAK <sellaktaha1@gmail.com>
2024-09-12 22:24:29 +0100

    Register Términer

--- src/app/authentication/signup/signup.component.ts
+++ src/app/authentication/signup/signup.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
+import {ProjetService} from "../../services/operations/register";
+import {AuthResponse} from "../../services/models/auth-response";
+import {UserDto} from "../../services/models/user-dto";
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  registerForm: FormGroup;
+  hidePassword = true;
+  hideConfirmPassword = true;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private service : ProjetService
+  ) {
+    this.registerForm = this.formBuilder.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required]]
+    }, { validator: this.checkPasswords });
+  }
+
+  ngOnInit(): void {
+  }
+
+  checkPasswords(group: FormGroup) {
+    const password = group.get('password')?.value;
+    const confirmPassword = group.get('confirmPassword')?.value;
+    return password === confirmPassword ? null : { passwordMismatch: true };
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log('Formulaire d\'inscription soumis', this.registerForm.value);
+      this.service.register(this.registerForm.value).subscribe(
+        (data: UserDto) => {
+          console.log('Login successful:', data);
+        },
+      );
+      this.router.navigate(['/']);
+    }
+  }
 }
