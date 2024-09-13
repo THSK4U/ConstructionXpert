045aef01336e982badb718ea25da4ed9939367a5
TAHA SELLAK <sellaktaha1@gmail.com>
2024-09-13 09:10:22 +0100

    Management component

--- src/app/management/management.module.ts
+++ src/app/management/management.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-import { ManagementRoutingModule } from './management-routing.module';
+import { ProjectComponent } from './project/project.component';
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {BrowserModule} from "@angular/platform-browser";
+import {AppComponent} from "../app.component";
+import {MatIconModule} from "@angular/material/icon";
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProjectComponent
+  ],
   imports: [
-    CommonModule,
-    ManagementRoutingModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule
+  ],
+  exports: [
+    ProjectComponent
   ]
 })
 export class ManagementModule { }
