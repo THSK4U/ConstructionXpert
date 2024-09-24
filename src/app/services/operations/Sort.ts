b5631852199504390483550d0cb159a26a8f106b
TAHA SELLAK <sellaktaha1@gmail.com>
2024-09-24 15:55:50 +0100

    Ajouter service de tri pour les projets

---
+++ src/app/services/operations/Sort.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import {ProjetsDto} from "../models/projets-dto";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SortService {
+
+
+  private apiUrl = 'http://localhost:8200';
+
+  constructor(private http:HttpClient) { }
+
+  public sort(field: string, direction: string): Observable<ProjetsDto[]> {
+    return this.http.get<ProjetsDto[]>(`${this.apiUrl}/sort/${field}/${direction}`);
+  }
+
+
+}
