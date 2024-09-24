e005506853fabe1c293519ac16c2f2c0efcab740
TAHA SELLAK <sellaktaha1@gmail.com>
2024-09-24 15:55:41 +0100

    Ajouter modules Angular Material dans ManagementModule

--- src/app/management/management.module.ts
+++ src/app/management/management.module.ts
@@ -6,18 +6,26 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {BrowserModule} from "@angular/platform-browser";
 import {AppComponent} from "../app.component";
 import {MatIconModule} from "@angular/material/icon";
+import {MatInputModule} from "@angular/material/input";
+import {MatDialogModule} from "@angular/material/dialog";
+import {FormsModule} from "@angular/forms";
+import { FormAjouterComponent } from './project/form-ajouter/form-ajouter.component';
 
 
 @NgModule({
   declarations: [
-    ProjectComponent
+    ProjectComponent,
+    FormAjouterComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatCardModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatInputModule,
+    MatDialogModule,
+    FormsModule
   ],
   exports: [
     ProjectComponent
